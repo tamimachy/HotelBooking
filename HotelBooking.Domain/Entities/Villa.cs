0c69e370f8ee4ced02d5757014e034af3817adaa
USER <tamimachy9897@gmail.com>
2025-02-27 23:03:34 +0600

    last save activities

--- HotelBooking.Domain/Entities/Villa.cs
+++ HotelBooking.Domain/Entities/Villa.cs
@@ -11,5 +11,11 @@ namespace HotelBooking.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupency { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
---
+++ HotelBooking.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Infrastructure.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
