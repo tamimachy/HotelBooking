a71cf1a07ce3117c594ad5aa485f7f2c5c9b8108
USER <tamimachy9897@gmail.com>
2025-05-10 20:02:42 +0600

    Add project files.

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,38 @@
+﻿using HotelBooking.Domain.Entities;
+using HotelBooking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
