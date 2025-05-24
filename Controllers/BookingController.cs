5ccfddbb32a8c58598f531768b9b1ad77b4ee4df
USER <tamimachy9897@gmail.com>
2025-05-24 23:31:45 +0600

    Booking Authorization.

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,6 +1,9 @@
 ﻿using HotelBooking.Application.Common.Interfaces;
+using HotelBooking.Application.Common.Utility;
 using HotelBooking.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace HotelBooking.Web.Controllers
 {
@@ -11,18 +14,49 @@ namespace HotelBooking.Web.Controllers
         {
             _unitOfWork = unitOfWork;
         }
+        [Authorize]
         public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ApplicationUser user = _unitOfWork.User.Get(u => u.Id == userId);
+
             Booking booking = new()
             {
                 VillaId = villaId,
                 Villa = _unitOfWork.Villa.Get(u=>u.Id==villaId,includeProperties: "VillaAmenity"),
                 CheckInDate = checkInDate,
                 Nights = nights,
-                CheckOutDate = checkInDate.AddDays(nights)
+                CheckOutDate = checkInDate.AddDays(nights),
+                UserId = userId,
+                Phone = user.PhoneNumber,
+                Email = user.Email,
+                Name = user.Name
             };
             booking.TotalCost = booking.Villa.Price * nights;
             return View(booking);
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult FinalizeBooking(Booking booking)
+        {
+            var villa = _unitOfWork.Villa.Get(u => u.Id == booking.VillaId);
+            booking.TotalCost = booking.Villa.Price * booking.Nights;
+
+            booking.Status = SD.StatusPending;
+            booking.BookingDate = DateTime.Now;
+
+            _unitOfWork.Booking.Add(booking);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
+        }
+
+        [Authorize]
+        public IActionResult BookingConfirmation(int bookingId)
+        {
+            return View(bookingId);
         }
     }
 }
