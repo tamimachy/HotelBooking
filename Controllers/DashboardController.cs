cdf217055a3aa71b9343601125b9b5b060436639
USER <tamimachy9897@gmail.com>
2025-07-07 09:19:31 +0600

    apexchart added in dashboard

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBooking.Application.Common.Interfaces;
+using HotelBooking.Application.Common.Utility;
+using HotelBooking.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
 
 namespace HotelBooking.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1;
+        public readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month-1,1);
+        public readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month,1);
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> GetTotalBookingRadialChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending ||
+            u.Status == SD.StatusCancelled);
+            var countByCurrentMonth = totalBookings.Count(u => u.BookingDate >= currentMonthStartDate && u.BookingDate <= DateTime.Now);
+            var countByPreviousMonth = totalBookings.Count(u => u.BookingDate >= previousMonthStartDate && u.BookingDate <= currentMonthStartDate);
+            RadialBarChartVM radialBarChartVM = new();
+            int incresedDecreasedRatio = 100;
+            if (countByPreviousMonth != 0)
+            {
+                incresedDecreasedRatio = Convert.ToInt32((countByCurrentMonth - countByPreviousMonth) / countByPreviousMonth * 100);
+            }
+            radialBarChartVM.TotalCount = totalBookings.Count();
+            radialBarChartVM.CountInCurrentMonth = countByCurrentMonth;
+            radialBarChartVM.HasRatioIncreased = currentMonthStartDate > previousMonthStartDate;
+            radialBarChartVM.Series = new int[] { incresedDecreasedRatio };
+
+            return Json(radialBarChartVM);
         }
     }
 }
---
+++ wwwroot/js/dashboard/getTotalBookingsRadial.js
@@ -0,0 +1,68 @@
+﻿$(document).ready(function () {
+    loadTotalBookingRadialChart();
+});
+function loadTotalBookingRadialChart() {
+    $(".chart-spinner").show();
+    $.ajax({
+        url: "/Dashboard/GetTotalBookingRadialChartData",
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            document.querySelector("#spanTotalBookingCount").innerHTML = data.totalCount;
+            var sectionCurrentCount = document.createElement("span");
+            if (data.hasRatioIncreased) {
+                sectionCurrentCount.className = "text-success me-1";
+                sectionCurrentCount.innerHTML = '<i class="bi bi-arrow-up-right-circle me-1"></i><span>' + data.countInCurrentMonth + '</span>';
+            }
+            else {
+                sectionCurrentCount.className = "text-danger me-1";
+                sectionCurrentCount.innerHTML = '<i class="bi bi-arrow-down-right-circle me-1"></i><span>' + data.countInCurrentMonth + '</span>';
+            }
+
+            document.querySelector("#sectionBookingCount").append(sectionCurrentCount);
+            document.querySelector("#sectionBookingCount").append("since last month");
+
+            loadRadialBarChart("totalBookingsRadialChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    });
+}
+function loadRadialBarChart(id, data) {
+    var options = {
+        chart: {
+            height: 280,
+            type: "radialBar"
+        },
+        series: [67],
+        plotOptions: {
+            radialBar: {
+                hollow: {
+                    margin: 15,
+                    size: "70%"
+                },
+
+                dataLabels: {
+                    showOn: "always",
+                    name: {
+                        offsetY: -10,
+                        show: true,
+                        color: "#888",
+                        fontSize: "13px"
+                    },
+                    value: {
+                        color: "#111",
+                        fontSize: "30px",
+                        show: true
+                    }
+                }
+            }
+        },
+        stroke: {
+            lineCap: "round",
+        },
+        labels: ["Progress"]
+    };
+    var chart = new ApexCharts(document.querySelector("#"+id), options);
+    chart.render();
+}
